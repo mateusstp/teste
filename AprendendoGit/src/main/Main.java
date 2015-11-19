26fab1616aefc797fd4173b0d25dbe28972e7839
socramteix <maquito_plar@hotmail.com>
2015-11-19 14:20:30 -0300

    Revert "l"
    This reverts commit 42a6aa0eb9e28cd2ac902869ce65f1fd20d8544c.

--- AprendendoGit/src/main/Main.java
+++ AprendendoGit/src/main/Main.java
@@ -16,7 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Teste arquivo uhuh");
+        System.out.println("Teste arquivo mateus");
     }
     
 }
